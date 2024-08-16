c2599d0183ffd86f6d6a62d0e5bb4981a553ba5e
KESHAV NARAYAN <keshavnarayan75@gmail.com>
2024-08-16 12:56:14 +0530

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+var typed = new Typed('#element', {
+        strings: ['Web Developer','Web Designer'],
+        typeSpeed: 50,
+});
+var typed2 =  new Typed('.text-gray', {
+        strings: ['know about me'],
+        typeSpeed: 50,
+});
+const visibility=document.getElementById("visible");
+const bdy=document.querySelector("body");
+const menu=document.querySelector("nav");
+const ftr=document.querySelector("footer");
+
+
+visibility.addEventListener("click",function(){
+        if(visibility.value==="Light"){
+                bdy.style.background="white";
+                bdy.style.color="black";
+                menu.style.color="white";
+                ftr.style.color="white";
+                ftr.style.backgroundColor="orange";
+                visibility.value="Dark";
+        }
+        else{
+                bdy.style.background="black";
+                bdy.style.color="white";
+                visibility.value="Light";
+        }
+
+});

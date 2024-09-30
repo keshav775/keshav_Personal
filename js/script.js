47c1f0f801da5711c5350f5cf7277c0489a047d5
KESHAV NARAYAN <keshavnarayan75@gmail.com>
2024-09-30 07:56:29 +0530

    update

--- js/script.js
+++ js/script.js
@@ -6,28 +6,3 @@ var typed2 =  new Typed('.text-gray', {
         strings: ['know about me'],
         typeSpeed: 50,
 });
-const visibility=document.getElementById("visible");
-const bdy=document.querySelector("body");
-const menu=document.querySelector("nav");
-const ftr=document.querySelector("footer");
-const navToggle = document.getElementById('nav-toggle');
-const navMenu = document.getElementById('nav-menu');
-
-
-visibility.addEventListener("click",function(){
-        if(visibility.value==="Light"){
-                bdy.style.background="white";
-                bdy.style.color="black";
-                menu.style.color="white";
-                ftr.style.color="white";
-                ftr.style.backgroundColor="orange";
-                visibility.value="Dark";
-        }
-        else{
-                bdy.style.background="black";
-                bdy.style.color="white";
-                visibility.value="Light";
-                ftr.style.backgroundColor="black";
-        }
-
-});
